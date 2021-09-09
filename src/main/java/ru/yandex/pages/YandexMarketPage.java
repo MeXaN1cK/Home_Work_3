58a8cce3b2a0b6398b378d75231cc1bdd9b5931e
MeXaN1cK <vojakastalker@gmail.com>
2021-09-09 19:50:10 +0300

    Added steps and Test for SmartPhones. Have some problems with "Nokia".

--- src/main/java/ru/yandex/pages/YandexMarketPage.java
+++ src/main/java/ru/yandex/pages/YandexMarketPage.java
@@ -1,13 +1,15 @@
 package ru.yandex.pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import java.util.Objects;
 
 import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.CollectionCondition.sizeLessThanOrEqual;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Configuration.timeout;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -20,6 +22,30 @@ public class YandexMarketPage {
     @Step("Переходим в категорию Ноутбуки")
     public void goToLaptops(){
         $x("//a[@href='/catalog--noutbuki/54544/list?hid=91013']").pressEnter();
+    }
+
+    @Step("Переходим в категорию Электроника")
+    public void goToElectronic(){
+        $x("//a[@href='/catalog--elektronika/54440']").pressEnter();
+    }
+
+    @Step("Переходим в категорию Смартфоны")
+    public void goToSmartphones(){
+        $x("//a[@href='/catalog--mobilnye-telefony/54726/list?hid=91491']").pressEnter();
+    }
+
+    @Step("Выбираем фильтры для поиска: Производитель {manufacturer}")
+    public void addFilters(String manufacturer){
+        if ($x("//legend[@class and contains(text(),'Производитель')]").is(visible)) {
+            if ($x("//legend[@class and contains(text(),'Производитель')]/parent::*/child::*/li/child::*//*[contains(text(),'"+manufacturer+"')]/parent::*/parent::*/parent::*/child::*").is(enabled)) {
+                $x("//legend[@class and contains(text(),'Производитель')]/parent::*/child::*/li/child::*//*[contains(text(),'" + manufacturer + "')]/parent::*/parent::*/parent::*/child::*").shouldBe(visible).click();
+            } else {
+                $x("//legend[@class and contains(text(),'Производитель')]/parent::*/child::*//*[contains(text(),'Показать всё')]").shouldBe(visible).pressEnter();
+                $x("//legend[@class and contains(text(),'Производитель')]/parent::*/child::*/child::*/input").setValue(manufacturer);
+                sleep(1500);
+                $x("//legend[@class and contains(text(),'Производитель')]/parent::*/child::*/child::*/child::*//*[contains(text(),'"+manufacturer+"')]/parent::*/parent::*/div/span").shouldBe(visible).shouldBe(enabled).click();
+            }
+        }
     }
 
     @Step("Выбираем фильтры для поиска: Производитель {manufacturer}, Цена от {from}, Цена до {to}")
@@ -35,12 +61,13 @@ public class YandexMarketPage {
 
     @Step("Ожидаем результатов поиска и проверяем на количество результатов {count}")
     public void waitSearchResult(Integer count){
-        sleep(timeout);
-        $x("//div[@data-apiary-widget-id='/content/results']").shouldBe(visible);
+        //sleep(timeout);
+        $x("//div[@data-apiary-widget-id='/content/results']").shouldBe(enabled).shouldBe(visible);
         $x("//button[@type='button' and @aria-haspopup]").shouldBe(visible).click();
         $x("//button[@class and contains(text(),'Показывать по 12')]").shouldBe(visible).click();
-        sleep(timeout);
-        $$x("//div[@data-zone-name='snippetList']/article").shouldHave(size(count));
+        $x("//div[@data-apiary-widget-id='/content/results']").shouldBe(enabled).shouldBe(visible);
+        //sleep(timeout);
+        $$x("//div[@data-zone-name='snippetList']/article").shouldHave(sizeLessThanOrEqual(count));
     }
 
     @Step("Ищем в поиске первый элемент из списка результатов")
@@ -50,5 +77,28 @@ public class YandexMarketPage {
         sleep(timeout);
         SelenideElement searchResult = $x("//div[@data-apiary-widget-id='/content/results']//*/article[1]//*/a[@href and @title]");
         searchResult.shouldHave(attribute("title",firstSearchElementName));
+    }
+
+    @Step("Проверяем результаты поиска по модели: {model} или производителю: {manufacturer}")
+    public void checkResults(String model, String manufacturer){
+        while ($x("//a[@aria-label='Следующая страница']").is(Condition.enabled)){
+            ElementsCollection results = $$x("//div[@data-zone-name='snippetList']/article");
+            results.forEach(el->
+                    el.$x("child::*/child::*//*/a[@href and @title]").shouldHave(Condition.attributeMatching("title","(.*?)(?i)("+model+"|"+manufacturer+")(.*?)"))
+            );
+            $x("//a[@aria-label='Следующая страница']").pressEnter();
+            $x("//div[@data-apiary-widget-id='/content/results']").shouldBe(enabled).shouldBe(visible);
+        }
+    }
+    @Step("Проверяем результаты поиска по модели: {model} или производителю: {manufacturer}")
+    public void checkResults(String manufacturer){
+        while ($x("//a[@aria-label='Следующая страница']").is(Condition.enabled)){
+            ElementsCollection results = $$x("//div[@data-zone-name='snippetList']/article");
+            results.forEach(el->
+                    el.$x("child::*/child::*//*/a[@href and @title]").shouldHave(Condition.attributeMatching("title","(.*?)(?i)("+manufacturer+")(.*?)"))
+            );
+            $x("//a[@aria-label='Следующая страница']").pressEnter();
+            $x("//div[@data-apiary-widget-id='/content/results']").shouldBe(enabled).shouldBe(visible);
+        }
     }
 }
--- src/test/java/ru/yandex/Tests.java
+++ src/test/java/ru/yandex/Tests.java
@@ -1,19 +1,22 @@
 package ru.yandex;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.yandex.pages.YandexMarketPage;
 import ru.yandex.pages.YandexPage;
 
+import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.timeout;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Tests {
 
-    YandexPage yandexPage = new YandexPage();
     YandexMarketPage yandexMarketPage = new YandexMarketPage();
 
     @BeforeEach
@@ -28,13 +31,21 @@ public class Tests {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl="https://yandex.ru/";
+    }
+
+    @AfterEach
+    public void close(){
+        closeWebDriver();
     }
 
     @Test
     public void TestLaptops(){
-        open("https://yandex.ru/");
-        yandexPage.goToMarket();
+        open(baseUrl,YandexPage.class)
+            .goToMarket();
+
         switchTo().window(1);
+
         yandexMarketPage.goToComputers();
         yandexMarketPage.goToLaptops();
         yandexMarketPage.addFilters("Lenovo",10000,30000);
@@ -42,5 +53,20 @@ public class Tests {
         yandexMarketPage.waitSearchResult(12);
         yandexMarketPage.searchFirstElement();
 
+    }
+
+    @ParameterizedTest(name="{displayName} {arguments}")
+    @CsvSource({"Apple","Google","HONOR","HUAWEI","Nokia","OnePlus","OPPO","realme","Samsung","vivo","Xiaomi","ZTE"})
+    public void TestSmartphones(String manufacturer){
+        open(baseUrl,YandexPage.class)
+                .goToMarket();
+
+        switchTo().window(1);
+
+        yandexMarketPage.goToElectronic();
+        yandexMarketPage.goToSmartphones();
+        yandexMarketPage.addFilters(manufacturer);
+        yandexMarketPage.waitSearchResult(12);
+        yandexMarketPage.checkResults(manufacturer);
     }
 }
